60965ec5b87ece4ef4a295b8c349d8e5a1b87fc5
Aldi Nugraha <aldinugrahap16@gmail.com>
2020-11-01 00:02:30 +0700

    first commit

---
+++ src/latihan18kelipatan/PBO10119908Latihan18.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan18kelipatan;
+
+/**
+ *
+ 
+ */
+public class PBO1010119908Latihan18 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        float kelipatan;
+        for(kelipatan =(float)3.5;kelipatan<=35.0;kelipatan+=3.5){
+            System.out.println(kelipatan);
+
+        }
+    }
+    
+}
